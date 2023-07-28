45bbc984a001e5ff4f6c54138a3312311580466d
Adeegithub <arunodagunawardana@gmail.com>
2023-07-28 16:07:11 +0530

    "Classes"

---
+++ src/Phone.java
@@ -0,0 +1,33 @@
+public class Phone {
+
+    String make;
+    String model;
+    String color;
+    int ram;
+
+    public void call(){
+        System.out.println("Dialing... Treeeeem Treeem...... from " + make + " " + model);
+    }
+
+    public void browseInternet () {
+        System.out.println("Browsing Internet..... from " + make + " " + model);
+    }
+
+    public static void main(String[] args) {
+        Phone mobile1 = new Phone();
+        mobile1.make = "Apple";
+        mobile1.model = "iPhone XS";
+        mobile1.color = "Space Grey";
+        mobile1.ram = 8;
+
+        mobile1.call();
+
+        Phone mobile2 = new Phone();
+        mobile2.make = "Samsung";
+        mobile2.model = "Galaxy S5";
+        mobile2.color = "Pitch Black";
+        mobile2.ram = 8;
+
+        mobile2.browseInternet();
+    }
+}
